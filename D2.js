99875d69aa72c8b0364418c3b7565815bd0cf284
fedemaso <fede.maso1989@gmail.com>
2023-05-30 17:01:41 +0200

    es 7

--- D2.js
+++ D2.js
@@ -71,7 +71,7 @@ if ((numb6 === 8) || (numb7 === 8)) {
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 
-const totalShoppingCart = 33;
+const totalShoppingCart = 77;
 
 if (totalShoppingCart > 50) {
   totalToPay = totalShoppingCart
@@ -111,6 +111,30 @@ console.log("Totale con sconto da addebitare all'utente: " + totalToPay);
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+
+var a = 7;
+var b = 3;
+var c = 10;
+
+if (a >= b && a >= c) {
+  if (b >= c) {
+    console.log(a, b, c);
+  } else {
+    console.log(a, c, b);
+  }
+} else if (b >= a && b >= c) {
+  if (a >= c) {
+    console.log(b, a, c);
+  } else {
+    console.log(b, c, a);
+  }
+} else if (c >= a && c >= b) {
+  if (a >= b) {
+    console.log(c, a, b);
+  } else {
+    console.log(c, b, a);
+  }
+}
 
 
 /* ESERCIZIO 8
